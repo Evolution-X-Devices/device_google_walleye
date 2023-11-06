5f1f14e4347884fa94d393025380e52bcf31ab4d
Asriadi Rahim <asriadirahim03@gmail.com>
2023-11-06 18:22:57 +0800

    walleye: proprietary: Regenerate

--- evolution_walleye.mk
+++ evolution_walleye.mk
@@ -38,7 +38,6 @@ $(call inherit-product, vendor/evolution/config/common_full_phone.mk)
 EVO_BUILD_TYPE := OFFICIAL
 TARGET_BOOT_ANIMATION_RES := 720
 TARGET_SUPPORTS_QUICK_TAP := true
-TARGET_USES_MINI_GAPPS := true
 TARGET_IS_PIXEL := true
 
 # Inherit product specific makefiles
